4168f5faad8614d1bc14eec1edf2f26538414a3c
Bois Gaetan <g.bois17@gmail.com>
2017-12-12 12:25:32 +0100

    test animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,2 +1,24 @@
-window.sr = ScrollReveal();
-sr.reveal('.fonctionnement');
+// Changing the defaults
+window.sr = ScrollReveal({ reset: true });
+
+// Customizing a reveal set
+sr.reveal('.text-home', { 
+    origin: 'top',
+    duration: 1500,
+    delay: 30,
+    distance: '10%',
+});
+
+sr.reveal('.presentation', { 
+    origin: 'left',
+    duration: 1500,
+    delay: 30,
+    distance: '10%',
+});
+
+sr.reveal('.fonctionnement', { 
+    origin: 'rigth',
+    duration: 1500,
+    delay: 30,
+    distance: '10%',
+});
